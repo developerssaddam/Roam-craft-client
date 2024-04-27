fc42c37b5f63ff0512a36e2a9cc3bdb032fd5e34
developersaddam <developerssaddam@gmail.com>
2024-04-27 18:15:25 +0600

    Added All Tourist spot page

--- src/components/TouristSection/TouristSection.jsx
+++ src/components/TouristSection/TouristSection.jsx
@@ -14,7 +14,7 @@ const TouristSection = () => {
         </p>
       </div>
 
-      <div className="tourist_card_container container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-8 p-4">
+      <div className="tourist_card_container container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-8">
         <TouristSpotCard />
         <TouristSpotCard />
         <TouristSpotCard />
--- src/pages/AllTourist/AllTourist.jsx
+++ src/pages/AllTourist/AllTourist.jsx
@@ -1,9 +1,19 @@
-
+import AllTouristCart from "../../components/AllTouristCart/AllTouristCart";
 
 const AllTourist = () => {
   return (
-    <div>AllTourist</div>
-  )
-}
+    <div className="tourist_card_container container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-8">
+      <AllTouristCart />
+      <AllTouristCart />
+      <AllTouristCart />
+      <AllTouristCart />
+      <AllTouristCart />
+      <AllTouristCart />
+      <AllTouristCart />
+      <AllTouristCart />
+      <AllTouristCart />
+    </div>
+  );
+};
 
-export default AllTourist
+export default AllTourist;
